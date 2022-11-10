a549489bfaba2824e7a3183cd0da8c7a18d59150
Kalina Buhleva <kalisb@abv.bg>
2022-11-10 20:23:09 +0200

    Add main functionallity

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
-import { Octokit as ActionsOctokit } from '@octokit/action';
+import * as http from '@actions/http-client'
+
+function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
+  if (val === undefined || val === null) {
+    throw new Error(
+      `Missing required environment value. Are you running in GitHub Actions?`
+    )
+  }
+}
 
 async function run(): Promise<void> {
   try {
     const dependencies: string[] = core.getMultilineInput('dependencies')
-    core.debug(`Extracting semantic trees from provided dependencies ...`) 
+    core.debug(
+      `Extracting semantic trees from provided dependencies ${dependencies[0]} ...`
+    )
     // get the repo name - required for authentication
-    const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
-    if (!GITHUB_REPOSITORY) {
-      throw new Error(`env.GITHUB_REPOSITORY not set`);
-    }
     // get latest commit
+    const {GITHUB_REPOSITORY, GITHUB_SHA} = process.env
+    assertIsDefined(GITHUB_REPOSITORY)
+    assertIsDefined(GITHUB_SHA)
     // call rest endpoint for evaluation
+    const defaultProviderEndpoint = `https://master.d2wyo5i4kvbomx.amplifyapp.com/gh/${GITHUB_REPOSITORY}/${GITHUB_SHA}`
+    const client = new http.HttpClient('actions-github-app-token')
+    client.get(defaultProviderEndpoint)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
